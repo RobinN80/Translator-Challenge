4d914c97422507771918898756dee0ae0847680f
Robin Novak <robin.m.novak@gmail.com>
2021-04-04 14:53:20 -0700

    initial commit

---
+++ src/Components/CompanyComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Companies extends Component {
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
